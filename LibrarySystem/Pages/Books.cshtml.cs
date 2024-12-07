26ecb3d296e48e82a67a56b071ccd08bc8c6ba24
OUSAMA-ELWA7ED <s-osama.kassem@zewailcity.edu.eg>
2024-12-07 21:48:55 +0200

    Update Books.cshtml.cs

--- LibrarySystem/Pages/Books.cshtml.cs
+++ LibrarySystem/Pages/Books.cshtml.cs
@@ -1,24 +1,24 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using LibrarySystem.Models; 
 using System.Collections.Generic;
-
+using LibrarySystem.Models;
 namespace LibrarySystem.Pages
 {
-    public class BooksModel : PageModel
+    public class AllBooksModel : PageModel
     {
-        public List<Book> AllBooks { get; set; } = new List<Book>();
+        public List<BookSection> BookSections { get; set; }
 
         public void OnGet()
         {
-            
-            AllBooks = new List<Book>
-            {
-                new Book { Id = 1, Title = "The Great Gatsby", Author = "F. Scott Fitzgerald", Genre = "Fiction", IsAvailable = true },
-                new Book { Id = 2, Title = "1984", Author = "George Orwell", Genre = "Dystopian", IsAvailable = true },
-                new Book { Id = 3, Title = "To Kill a Mockingbird", Author = "Harper Lee", Genre = "Classic", IsAvailable = true },
-                new Book { Id = 4, Title = "Pride and Prejudice", Author = "Jane Austen", Genre = "Romance", IsAvailable = false },
-                new Book { Id = 5, Title = "Moby Dick", Author = "Herman Melville", Genre = "Adventure", IsAvailable = true }
-            };
+            BookSections = new List<BookSection> { };
         }
     }
+
+    public class BookSection
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Book> Books { get; set; } = new List<Book>();
+    }
+
+    
 }
