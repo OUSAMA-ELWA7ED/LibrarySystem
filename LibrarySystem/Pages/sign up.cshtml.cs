b7a9f36690f382e3c878f345081dd752d4f3cc1d
Hema-1 <s-ebrahim.khalifa@zewailcity.edu.eg>
2024-12-07 20:49:37 +0200

    Update sign up.cshtml.cs

--- LibrarySystem/Pages/sign up.cshtml.cs
+++ LibrarySystem/Pages/sign up.cshtml.cs
@@ -19,14 +19,14 @@ namespace Library_Project.Pages
 
         public IActionResult OnPost()
         {
-            // Handle form submission logic here
+           
             if (!ModelState.IsValid)
             {
                 return Page();
             }
 
             // Save the form data to a database or perform other operations
-            return RedirectToPage("Success"); // Redirect to a success page
+            return RedirectToPage("Index"); 
         }
     }
 }
